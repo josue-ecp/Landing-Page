0eb5c6d6e21869a03bbb0c80cc99cf20ad544e5b
josue <josueceh1@gmail.com>
2024-08-29 18:35:10 -0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Holkan - Start Bootstrap Template</title>
+    
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+   
+    <link href="css/styles.css" rel="stylesheet" />
+    <style>
+        .logo-small {
+            width: 100px; 
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .team-card {
+            color: black;
+            background-color: #ffffff; /* Fondo blanco para el texto */
+            border-radius: 15px; /* Opcional: Redondea las esquinas de la tarjeta */
+            padding: 10px; /* Añadir algo de padding para que el texto no esté pegado a los bordes */
+        }
+        .product-card {
+            background-color: #f8f9fa; /* Fondo claro para la tarjeta */
+            color: #212529; /* Color oscuro para el texto */
+            border: 1px solid #dee2e6; /* Borde ligero */
+            border-radius: 10px; /* Bordes redondeados */
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra ligera */
+            padding: 20px; /* Padding interno */
+        }
+        .product-card h2 {
+            font-size: 1.5rem; /* Tamaño de fuente del título */
+        }
+        .product-card p {
+            font-size: 1rem; /* Tamaño de fuente del texto */
+        }
+    </style>
+</head>
+<body style="background-color: #004d00; color: white;">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="#!">Holkan</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Acerca de</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contacto</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Servicios</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Todo el contenido de la pagina-->
+    <div class="container px-4 px-lg-5">
+        <!-- encabezado-->
+        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+            <div class="col-lg-7">
+                <img class="img-fluid rounded mb-4 mb-lg-0" src="assets/holkan.jpg" alt="..." />
+            </div>
+            <div class="col-lg-5 text-center">
+                <img src="assets/logo.png" alt="Logo de Holkan" class="img-fluid mb-3 logo-small" />
+                <h1 class="font-weight-light">Holkan</h1>
+                <p>Embárcate en una aventura épica en Xibalbá, el oscuro inframundo maya, con Holkan como tu valiente protagonista. En este desafiante juego de plataformas, deberás recolectar ofrendas sagradas para liberarte de una maldición ancestral y escapar del temido inframundo. A lo largo de tu travesía, enfrentarás obstáculos y enemigos, mientras exploras un entorno vibrante y lleno de mitología maya. Prepárate para una experiencia única que combina acción emocionante con elementos educativos, todo en un mundo fascinante inspirado en la rica cultura de los antiguos mayas.</p>
+                <a class="btn btn-primary" href="https://ghostvdz.itch.io/holkan">Prueba Holkan!</a>
+            </div>
+        </div>
+        <!-- esta es la parte de los productos-->
+        <div class="row gx-4 gx-lg-5">
+            <div class="col-md-4 mb-5">
+                <div class="card h-100 product-card">
+                    <img src="assets/tote mayab.png" class="card-img-top" alt="Producto 1">
+                    <div class="card-body">
+                        <h2 class="card-title">Tote Mayab</h2>
+                        <p class="card-text">Mayab, el nombre en maya de la península de Yucatán y también el de nuestra bolsa tejida de Henequén, la fibra resistente del agave nativo de la zona.</p>
+                        <p class="card-text">$380.00</p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Más Info</a></div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5">
+                <div class="card h-100 product-card">
+                    <img src="assets/contenedor.png" class="card-img-top" alt="Producto 2">
+                    <div class="card-body">
+                        <h2 class="card-title">contenedor Cantera</h2>
+                        <p class="card-text">El contenedor Cantera es una pieza donde las técnicas artesanales y el Diseño contemporáneo convergen. Fabricada con la técnica de tallado de piedra, nos demuestra que la cultura maya viva tiene un lugar importante en la vida actual.</p>
+                        <p class="card-text">$1,375.00</p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Más Info</a></div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5">
+                <div class="card h-100 product-card">
+                    <img src="assets/xibalba.png" class="card-img-top" alt="Producto 3">
+                    <div class="card-body">
+                        <h2 class="card-title">Xibalba</h2>
+                        <p class="card-text">Te presentamos a Xibalba, el señor del inframundo Maya...
+
+                            Esta pieza alebrije es la conjunción de tres animales representativos en la cosmovisión de los pueblos mayas: el jaguar, el murciélago y la serpiente. Los símbolos que cubren su piel son la representación de las 6 cuevas que conforman el inframundo: la del fuego, la de las navajas, la del frio, la de la oscuridad, la de los murciélagos y la del jaguar. </p>
+                        <p class="card-text">$56,072.00</p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Más Info</a></div>
+                </div>
+            </div>
+        </div>
+            <!-- aqui esta la informacion del los integrantes del equipo-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-12 text-center">
+                    <h2>Conoce al Equipo</h2>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100 text-center team-card">
+                        <img src="assets/yo.jpg" class="card-img-top rounded-circle mx-auto d-block" alt="Equipo 1">
+                        <div class="card-body">
+                            <h4 class="card-title">Josue Ceh</h4>
+                            <p class="card-text">Contribuyó significativamente en el desarrolo del video juego y  con el diseño y creacion de la landing page.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100 text-center team-card">
+                        <img src="assets/haziel.jpg" class="card-img-top rounded-circle mx-auto d-block" alt="Equipo 2">
+                        <div class="card-body">
+                            <h4 class="card-title">Haziel Montejo</h4>
+                            <p class="card-text">Contribuyó significativamente al diseño de personajes , assets, sprites, etc. y desarrollo.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100 text-center team-card">
+                        <img src="assets/flavio1.png" class="card-img-top rounded-circle mx-auto d-block" alt="Equipo 3">
+                        <div class="card-body">
+                            <h4 class="card-title">Flavio Valdez</h4>
+                            <p class="card-text"> Ayudó en la programación y la implementación de mecánicas de juego.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- galeria del juego-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-12 text-center">
+                    <h2>Galería del Juego</h2>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/power ups.png" alt="power ups" />
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/personajes.png" alt="Personajes del juego" />
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/escenarios.png" alt="Escenarios del juego" />
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/menu pausa.png" alt="Menu de pausa" />
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/game over.png" alt="Game Over" />
+                </div>
+                <div class="col-md-4 mb-5">
+                    <img class="img-fluid rounded mb-4" src="assets/mecanicas.png" alt="Mecanicas" />
+                </div>
+            </div>
+        </div>
+        <!-- copyright-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Holkan 2024</p></div>
+        </footer>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
